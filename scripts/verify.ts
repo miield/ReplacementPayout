d4437af142e9d3b051f0633ea16ddcbd3ba161df
miield <aodasola95@gmail.com>
2022-07-10 01:00:24 +0100

    verify script

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -2,3 +2,19 @@ import { run, ethers } from "hardhat";
 
 // connecting to the .env
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS ?? "";
+
+async function verify() {
+    await run("verify:verify", {
+        constructorArguments: [
+            "Danii",
+            "DANII",
+            ethers.utils.parseUnits("31801783.659820959115981", 18),
+            18,
+        ],
+    });
+}
+
+verify().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
